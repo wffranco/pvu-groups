10f08161a184c459eb433b5d7eb91b197c986e52
Willem Franco <willem.franco@blankfactor.com>
2021-09-23 09:10:17 -0500

    Updates

---
+++ precache-manifest.17f0d5f3a8d156f70ada88b09d48546f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f629305255d6ee681cfe",
+    "url": "/css/app.26bb9a24.css"
+  },
+  {
+    "revision": "757b5d2722f2598799e7cacb52e6e3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f629305255d6ee681cfe",
+    "url": "/js/app.57e9438d.js"
+  },
+  {
+    "revision": "30b4673cf6d0d0e30e17",
+    "url": "/js/chunk-vendors.e4120ca2.js"
+  },
+  {
+    "revision": "5159fcf15643a3dd8d35615a43e426d9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
